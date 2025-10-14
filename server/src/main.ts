17c5b18bc631aaa637305eff9450e69157f1e4b3
whiteheart313 <ammarhamed64@gmail.com>
2025-10-14 16:17:44 +0300

    add: Implement global exception handling and logging with Winston

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './filter/all-exceptions.filter';
+
+(async () => {
+  console.log('Starting ledger-task service...');
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    // if there is a client is going to talk to you.
+      origin: ['http://localhost:5173', 'http://frontend:5173'],
+      methods: ['GET', 'POST'],
+      credentials: true,
+    });
+
+  console.log('Configuring global validation pipe...')
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }));
+  
+
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+})();
