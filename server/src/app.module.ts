7e8ca35800c47442b974cde2c16412d775dfb3dc
whiteheart313 <ammarhamed64@gmail.com>
2025-10-16 00:26:05 +0300

    update app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ApartmentModule } from './apartment/apartment.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    ApartmentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
