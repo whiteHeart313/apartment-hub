6194f70c40930cd3a26a7d08aed0c965a9d28bc1
whiteheart313 <ammarhamed64@gmail.com>
2025-10-16 02:20:45 +0300

    refactor: update type imports and simplify module alias check

--- server/src/alias-register.ts
+++ server/src/alias-register.ts
@@ -1,6 +1,6 @@
 // src/alias-register.ts
 // Intentionally using require for module-alias registration.
 // This file will be ignored by the linter.
-if (process.env.NODE_ENV !== 'development' || !process.env.IS_TS_NODE) {
+if (process.env.NODE_ENV !== 'development') {
   require('module-alias/register');
 }
