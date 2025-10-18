5ef27a690f03885b937201b5eaaea850aa697f2b
whiteheart313 <ammarhamed64@gmail.com>
2025-10-18 15:42:44 +0300

    Update(header) : remove ApartmentListing header

--- client/src/components/ApartmentListingPage.tsx
+++ client/src/components/ApartmentListingPage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useApartmentFilters } from "../hooks/useApartmentFilters";
-import { ApartmentListingHeader } from "./ApartmentListingHeader";
 import { ApartmentListingContent } from "./ApartmentListingContent";
 import { SearchBar } from "./SearchBar";
 
@@ -24,8 +23,6 @@ export function ApartmentListingPage() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <ApartmentListingHeader />
-
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <SearchBar
           searchQuery={filters.searchQuery}
